e5a7d3c2eb5c24be347d8b3aefee67bb6750f7ac
Autumn Farris <autumn@autumnfarris.com>
2020-08-30 23:15:08 -0500

    Add router

---
+++ autumnfarris.com/my-app/src/components/Resume.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+  render() {
+    return (
+    <div className="Resume">
+      <h1>Resume</h1>
+    </div>
+    );
+  }
+}
+
+export default Resume;
